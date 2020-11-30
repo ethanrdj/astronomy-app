2aa437784f8210dcff71b5a8fea29d46e8c6159b
ethanrdj <ethanrdavis96@gmail.com>
2020-11-30 17:08:07 +0000

    added animated button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link as LinkRRD, useLocation } from "react-router-dom";
 import { Link } from "react-scroll";
-import ReorderIcon from "@material-ui/icons/Reorder";
 
 import "../styles/NavBar.css";
 
@@ -21,26 +20,41 @@ const NavBar = () => {
             offset={0}
             duration={500}
           >
-            <li className="nav-item" onClick={() => setShowLinks(!showLinks)}>
+            <li
+              className="nav-item"
+              id={showLinks ? "hidden-nav-item" : ""}
+              onClick={() => setShowLinks(!showLinks)}
+            >
               APOD
             </li>
           </Link>
         ) : (
           <LinkRRD to="/">
-            <li className="nav-item" onClick={() => setShowLinks(!showLinks)}>
+            <li
+              className="nav-item"
+              id={showLinks ? "hidden-nav-item" : ""}
+              onClick={() => setShowLinks(!showLinks)}
+            >
               HOME
             </li>
           </LinkRRD>
         )}
         <LinkRRD to="/solarsystem">
-          <li className="nav-item" onClick={() => setShowLinks(!showLinks)}>
+          <li
+            className="nav-item"
+            id={showLinks ? "hidden-nav-item" : ""}
+            onClick={() => setShowLinks(!showLinks)}
+          >
             OUR SOLAR SYSTEM
           </li>
         </LinkRRD>
       </ul>
-      <button onClick={() => setShowLinks(!showLinks)}>
-        <ReorderIcon />
-      </button>
+      <div
+        className={!showLinks ? "menu-btn" : "menu-btn open"}
+        onClick={() => setShowLinks(!showLinks)}
+      >
+        <div className="menu-btn__burger"></div>
+      </div>
     </div>
   );
 };
